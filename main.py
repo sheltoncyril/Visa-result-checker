1eebd12f30115f79eebea4c98ddff3d51258ec49
Shelton Cyril <shelton@encyclofoodia.com>
2023-06-18 01:18:03 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import pdf_downloader
+import search_pdfs
+
+FOLDER = "./temp"
+SEARCH_IDS = []
+
+folder_path = Path(FOLDER)
+if not folder_path.exists():
+    folder_path.mkdir(parents=True, exist_ok=True)
+print("Starting download")
+file_paths = pdf_downloader(folder_path)
+print("Downloads complete. Processing PDFs.")
+results = search_pdfs(folder_path, file_paths, SEARCH_IDS)
+print("Processed PDFs.")
+print(f"Results: {results.to_string()}")
---
+++ pdf_downloader.py
@@ -0,0 +1,39 @@
+import sys
+from pathlib import Path
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def pdf_downloader(folder):
+    downloaded = 0
+    cached = 0
+    folder_path = Path(folder).joinpath("pdfs")
+    if not folder_path.exists():
+        folder_path.mkdir(parents=True, exist_ok=True)
+    base_url = "https://www.dfa.ie"
+    url = "/irish-embassy/india/visas/processing-times-decisions-appeals/"
+    file_paths = list()
+    page = requests.get(base_url+url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find(id="tab2").find_all("a", href=True)[1:]
+    file_name_to_url_map = {r["href"].split("/")[-1]: r['href'] for r in results}
+    for filename, url in file_name_to_url_map.items():
+        file_path = folder_path.joinpath(filename)
+        if not file_path.exists():
+            f = requests.get(base_url+url)
+            with open(file_path, "wb") as fb:
+                fb.write(f.content)
+                downloaded += 1
+        else:
+            cached += 1
+        file_paths.append(file_path)
+        print(f"Downloaded: {downloaded}, Cached: {cached}", end="\r")
+    print(f"\nTotal files: {downloaded+cached}")
+    return file_paths
+
+
+if __name__ == "__main__":
+    print("This file cannot be run as a script.")
+else:
+    sys.modules[__name__] = pdf_downloader
